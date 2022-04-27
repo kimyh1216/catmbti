b84495856fef739f36577ef62ee6accb082713df
kimyh1216 <kimyh1216@gmail.com>
2022-04-27 17:39:25 +0900

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,56 @@
+import React from 'react'
+//css-in-js
+import styled from 'styled-components'
+import CatImage from '../asset/cat_image.jpg'
+import Button from 'react-bootstrap/Button'
+import { useNavigate } from 'react-router-dom'
+
+const Home = () => {
+  const navigate = useNavigate()
+  const handleClickButton = () => {
+    navigate('/question')
+  }
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Contents>
+        <Title>나에게 맞는 주인님은?</Title>
+        <LogoImage>
+          <img src={CatImage} className="rounded-circle" width={350} height={350} alt="고양이 로고" />
+        </LogoImage>
+        <Desc className="text-primary">MBTI를 기반으로 하는 나랑 잘맞는 고양이 찾기!</Desc>
+        <Button style={{ fontFamily: "SimKyungha" }} onClick={handleClickButton}>테스트 시작하기</Button>
+      </Contents>
+    </Wrapper>
+  )
+}
+
+export default Home
+
+const Wrapper = styled.div`
+  height: 100vh;
+  font-family: "SimKyungha";
+`
+const Header = styled.div`
+  font-size: 40pt;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+const Contents = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+`
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+`
+const LogoImage = styled.div`
+  margin-top: 10px;
+`
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+`
---
+++ src/pages/Result.js
@@ -0,0 +1,62 @@
+import React from 'react'
+//css-in-js
+import styled from 'styled-components'
+import Button from 'react-bootstrap/Button'
+import { ResultData } from '../asset/data/resultdata'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+
+const Result = () => {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const mbti = searchParams.get('mbti')
+  const [resultData, setResultData] = React.useState({})
+
+  React.useEffect(() => {
+    const result = ResultData.find((s) => s.best === mbti)
+    setResultData(result)
+  }, [mbti])
+  console.log(resultData)
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Contents>
+        <Title>결과 보기</Title>
+        <LogoImage>
+          <img src={resultData.image} className="rounded-circle" width={350} height={350} alt="고양이 로고" />
+        </LogoImage>
+        <Desc className="text-primary">예비 집사님과 찰떡궁합인 고양이는 {resultData.name}입니다.</Desc>
+        <Button style={{ fontFamily: "SimKyungha" }} onClick={() => navigate('/')}>테스트 다시하기</Button>
+      </Contents>
+    </Wrapper >
+  )
+}
+
+export default Result
+
+const Wrapper = styled.div`
+  height: 100vh;
+  font-family: "SimKyungha";
+`
+const Header = styled.div`
+  font-size: 40pt;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`
+const Contents = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+`
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+`
+const LogoImage = styled.div`
+  margin-top: 10px;
+`
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+`
