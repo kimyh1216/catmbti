433123c476d9434150fe77c5f3af8edb2ecf2ad3
kimyh1216 <kimyh1216@gmail.com>
2022-08-30 17:22:05 +0900

    add kakao message

--- src/Component/KakaoShareButton.js
+++ src/Component/KakaoShareButton.js
@@ -2,9 +2,10 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 const {Kakao} = window;
 
-const KakaoShareButton = () =>{
+const KakaoShareButton = ({data}) =>{
     const url = "https://catmbti1234.netlify.app"
     const resultUrl = window.location.href;
+    console.log('data',data)
     console.log('aaa',resultUrl,url)
     React.useEffect(()=>{
         Kakao.cleanup()
@@ -16,12 +17,11 @@ const KakaoShareButton = () =>{
           objectType: 'feed',
           content: {
             title: '예비집사 판별기 결과',
-            description: '예비 집사님이 고양이를 키운다면 가장 잘 맞는 고양이는?',
-            imageUrl:
-              'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+            description: `예비 집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name} 입니다.`,
+            imageUrl: url + data.image,
             link: {
-              mobileWebUrl: 'https://developers.kakao.com',
-              androidExecutionParams: 'test',
+              mobileWebUrl: resultUrl,
+              webUrl : resultUrl
             },
           },
           buttons: [
@@ -29,6 +29,7 @@ const KakaoShareButton = () =>{
               title: '나도 테스트 하러가기',
               link: {
                 mobileWebUrl: url,
+                webUrl: url
               },
             }
           ]
